2a02723b21ea7e0d99b06f2ac11a9478c69fc517
stark5200 <a.taha5200@gmail.com>
2023-12-31 12:51:50 +0100

    lesson0.21

--- chapter_1/main.rs
+++ chapter_1/main.rs
@@ -5,9 +5,6 @@ fn main() {
     let stdout = stdout();
     let message = String::from("Hello fellow Rustaceans!");
     let width = message.chars().count();
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
 
     let mut writer = BufWriter::new(stdout.lock());
     say(&message, width, &mut writer).unwrap();
