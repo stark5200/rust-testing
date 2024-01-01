070a9b2a5ef6adad025104ed1662945b37414a9d
stark5200 <a.taha5200@gmail.com>
2024-01-01 20:35:57 +0100

    guessing game done

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,23 +7,30 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=10);
     
-    println!("Please input your guess");
+    loop {
+        println!("Please input your guess");
 
-    let mut guess = String::new();  
+        let mut guess = String::new();  
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line"); //.read_line() returns a Result of type Enumeration or enum, enum == Err or Ok, pass enum into.expect() to handle error
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line"); //.read_line() returns a Result of type Enumeration or enum, enum == Err or Ok, pass enum into.expect() to handle error
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    print!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too small!"),
-        Ordering::Equal => println!("You win!🥳🥳!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too Great!"),
+            Ordering::Equal => {
+                println!("You win!🥳🥳!");
+                break;
+            }
+        }
     }
-
     println!("the secret number is: {secret_number}");
 }
