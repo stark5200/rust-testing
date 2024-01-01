556c84c893c84918ab86ce5e1e50c91f5caea82f
stark5200 <a.taha5200@gmail.com>
2024-01-01 20:26:13 +0100

    Ordering

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -14,6 +15,15 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line"); //.read_line() returns a Result of type Enumeration or enum, enum == Err or Ok, pass enum into.expect() to handle error
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     print!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too small!"),
+        Ordering::Equal => println!("You win!🥳🥳!"),
+    }
+
     println!("the secret number is: {secret_number}");
 }
