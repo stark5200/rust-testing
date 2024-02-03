5313683f549a0d51209d70a18b2bc9aa10e44996
stark5200 <a.taha5200@gmail.com>
2024-02-04 00:47:31 +0100

    custom fail message

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -2,6 +2,14 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+pub fn greeting(name: &str) -> String {
+    String::from("Hello!")//format!("Hello {}!", name)
+}
+
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
@@ -55,5 +63,20 @@ mod tests {
     #[test]
     fn failed() {
         panic!("make this test fail");
+    }
+
+    #[test] 
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test] 
+    fn greeting_contains_name() {
+        let result = greeting("Stark");
+        assert!(
+            result.contains("Stark"), 
+            "Greeting did not contain name, value was `{}`", 
+            result
+        );
     }
 }
