533b40421647b8e3420c89aa24d251ad32c6e954
stark5200 <a.taha5200@gmail.com>
2024-02-04 01:28:07 +0100

    11.1 done

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -22,6 +22,21 @@ impl Rectangle {
     }
 }
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!("Guess value must be greater than or equal to 1, got {}.", value);
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {}.", value);
+        } 
+        Guess { value }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -78,5 +93,11 @@ mod tests {
             "Greeting did not contain name, value was `{}`", 
             result
         );
+    }
+
+    #[test]
+    #[should_panic(expected = "less than or equal to 100")] 
+    fn greater_than_100() {
+        Guess::new(-200);
     }
 }
