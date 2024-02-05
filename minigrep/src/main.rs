008761e3f847f42e1a105bdf45ea80f55782ea41
stark5200 <a.taha5200@gmail.com>
2024-02-05 08:28:12 +0100

    struct

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,14 +4,28 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = Config::new(&args);
+    //let (query, file_path) = parse_config(&args);
 
-    println!("searching for {}", query);
-    println!("in file {}", file_path);
+    println!("searching for {}", config.query);
+    println!("in file {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Should be able to read file");
+    let contents = fs::read_to_string(config.file_path).expect("Should be able to read file");
 
     println!("With text:\n{contents}");
     //dbg!(args);
+}
+
+struct Config {
+    query: String, 
+    file_path: String, 
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
