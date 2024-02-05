6805e847cfda0e591e1cf27e2336ddd65fd92496
stark5200 <a.taha5200@gmail.com>
2024-02-05 03:13:12 +0100

    searching for file

---
+++ minigrep/target/debug/deps/minigrep-5856eb26c24feb18.d
@@ -0,0 +1,5 @@
+/home/stark5200/webdev/rust-testing/minigrep/target/debug/deps/minigrep-5856eb26c24feb18: src/main.rs
+
+/home/stark5200/webdev/rust-testing/minigrep/target/debug/deps/minigrep-5856eb26c24feb18.d: src/main.rs
+
+src/main.rs:
---
+++ minigrep/target/debug/minigrep.d
@@ -0,0 +1,1 @@
+/home/stark5200/webdev/rust-testing/minigrep/target/debug/minigrep: /home/stark5200/webdev/rust-testing/minigrep/src/main.rs
