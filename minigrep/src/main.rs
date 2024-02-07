35e12d4df11fb2e66c64229ded41217f7eb89bf7
stark5200 <a.taha5200@gmail.com>
2024-02-07 23:33:44 +0100

    extra

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -20,5 +20,10 @@ fn main() {
         process::exit(1);
     };
     //dbg!(args);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    };
+    //dbg!(args);
 }
 
