dc5ac38d79001ddea83e583906f0a0bb320d0adf
stark5200 <a.taha5200@gmail.com>
2024-02-12 02:52:37 +0100

    12.4

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -22,7 +22,11 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    //remove     println!("With text:\n{contents}");
+    
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
     Ok(())
 }
 
@@ -34,26 +38,24 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-            Rust:
-            safe, fast, productive.
-            Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
-}
 
-// pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-//     let mut results = Vec::new();
-// 
-//     for line in contents.lines() {
-//         if line.contains(query) {
-//             results.push(line);
-//         }
-//     }
-// 
-//     results
-// }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
