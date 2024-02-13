22e4cc627285976a025496b16d036a573b5f1efc
stark5200 <a.taha5200@gmail.com>
2024-02-13 21:41:16 +0100

    done with 12

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -7,32 +7,18 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        //remove     println!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
-    //let (query, file_path) = parse_config(&args);
-
-    //remove     println!("searching for {}", config.query);
-    //remove     println!("in file {}", config.file_path);
-
-    if let Err(e) = minigrep::run(config) {
-        //remove     println!("Application error: {e}");
-        process::exit(1);
-    };
-    //dbg!(args);
-
-    /*
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
+    //let (query, file_path) = parse_config(&args);
+    //remove     println!("searching for {}", config.query);
+    //remove     println!("in file {}", config.file_path);
+    //dbg!(args);
+    
     if let Err(e) = minigrep::run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
-    }
-    */
+    };
 }
 
