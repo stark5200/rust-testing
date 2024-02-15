01469336e2dc6a079c01601d96eac65c4857d273
stark5200 <a.taha5200@gmail.com>
2024-02-15 21:48:31 +0100

    example closure

--- thirteen/src/main.rs
+++ thirteen/src/main.rs
@@ -61,3 +61,8 @@ fn  add_one_v1   (x: u32) -> u32 { x + 1 }
 let add_one_v2 = |x: u32| -> u32 { x + 1 };
 let add_one_v3 = |x|             { x + 1 };
 let add_one_v4 = |x|               x + 1  ;
+
+let example_closure = |x| x;
+
+let s = example_closure(String::from("hello"));
+let n = example_closure(5);
