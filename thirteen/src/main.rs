5a585aa1ca03be008e071c6e22e2b70222dc450d
stark5200 <a.taha5200@gmail.com>
2024-02-15 21:07:41 +0100

    add

--- thirteen/src/main.rs
+++ thirteen/src/main.rs
@@ -56,3 +56,8 @@ let expensive_closure = |num: u32| -> u32 {
     thread::sleep(Duration::from_secs(2));
     num
 };
+
+fn  add_one_v1   (x: u32) -> u32 { x + 1 }
+let add_one_v2 = |x: u32| -> u32 { x + 1 };
+let add_one_v3 = |x|             { x + 1 };
+let add_one_v4 = |x|               x + 1  ;
