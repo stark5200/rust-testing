95e2ceda861c4ad10538fca962f839e76ee3af77
stark5200 <a.taha5200@gmail.com>
2024-02-15 21:07:08 +0100

    expensive closure

--- thirteen/src/main.rs
+++ thirteen/src/main.rs
@@ -50,3 +50,9 @@ fn main() {
         user_pref2, giveaway2
     );
 }
+
+let expensive_closure = |num: u32| -> u32 {
+    println!("calculating slowly...");
+    thread::sleep(Duration::from_secs(2));
+    num
+};
