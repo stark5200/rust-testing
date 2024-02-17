9793d903e79a97c7e33980300747442840c5c094
stark5200 <a.taha5200@gmail.com>
2024-02-17 22:53:55 +0100

    explanation

--- thirteen/src/main.rs
+++ thirteen/src/main.rs
@@ -59,11 +59,14 @@ let expensive_closure = |num: u32| -> u32 {
     num
 };
 
+/* explaining how functions and closures can be defined differently 
 fn  add_one_v1   (x: u32) -> u32 { x + 1 }
 let add_one_v2 = |x: u32| -> u32 { x + 1 };
 let add_one_v3 = |x|             { x + 1 };
 let add_one_v4 = |x|               x + 1  ;
+ */
 
+/* more examples to explain  
 let example_closure = |x| x;
 
 let s = example_closure(String::from("hello"));
@@ -78,4 +81,4 @@ fn main() {
     println!("Before calling closure: {:?}", list);
     only_borrows();
     println!("After calling closure: {:?}", list);
-}
+} */
