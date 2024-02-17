656a997488b17c8e57d549caf5c8becd7a731d80
stark5200 <a.taha5200@gmail.com>
2024-02-17 23:19:35 +0100

    done with closures

--- thirteen/src/main.rs
+++ thirteen/src/main.rs
@@ -33,6 +33,18 @@ impl Inventory {
     }
 }
 
+impl <T> Option<T> {
+    pub fn unwrap_or_else<F>(self, f:F) -> T
+    where
+        F: FnOnce() -> The
+    {
+        match self {
+            Some(x) => x, 
+            None => f(), 
+        }
+    } 
+} 
+
 fn main() {
     let store = Inventory {
         shirts: vec![ShirtColor::Blue, ShirtColor::Red, ShirtColor::Blue],
@@ -89,6 +101,28 @@ fn main() {
     println!("Before defining closure: {:?}", list);
 
     thread::spawn(move || println!("From thread: {:?}", list)).join().unwrap();
+}
+
+*/
+
+// Moving captured values out of closures and the Fn traits
+
+/* Rectangle example
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn main() {
+    let mut list = [
+        Rectangle { width: 10, height: 1 },
+        Rectangle { width: 3, height: 5 },
+        Rectangle { width: 7, height: 12 },
+    ];
+
+    list.sort_by_key(|r| r.width);
+    println!("{:#?}", list);
 }
 
 */
