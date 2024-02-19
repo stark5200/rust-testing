d2d7f6f3329ae8e32f154fed30f3b5e1efa2155f
stark5200 <a.taha5200@gmail.com>
2024-02-19 20:46:30 +0100

    trait

--- thirteen/src/main.rs
+++ thirteen/src/main.rs
@@ -126,11 +126,19 @@ fn main() {
 }
 
 */
-// 13.2
+// 13.2 
 let v1 = vec![1, 2, 3];
 
 let v1_iter = v1.iter();
 
 for val in v1_iter {
     println!("Got: {}", val);
+}
+
+pub trait Iterator {
+    type Item;
+
+    fn next(&mut self) -> Option<Self::Item>;
+
+    // methods with default implementations elided
 }
