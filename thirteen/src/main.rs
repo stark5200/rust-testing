ee6373ecf794f01e5f785337925e5867694f2cf2
stark5200 <a.taha5200@gmail.com>
2024-02-20 01:42:42 +0100

    iterator demo

--- thirteen/src/main.rs
+++ thirteen/src/main.rs
@@ -142,3 +142,15 @@ pub trait Iterator {
 
     // methods with default implementations elided
 }
+
+#[test]
+fn iterator_demonstration() {
+    let v1 = vec![1, 2, 3];
+
+    let mut v1_iter = v1.iter();
+
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+}
