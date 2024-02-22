36fdb103dd081d23050e7c8853dad4ba06165907
stark5200 <a.taha5200@gmail.com>
2024-02-22 22:02:05 +0100

    assert

--- thirteen/src/main.rs
+++ thirteen/src/main.rs
@@ -159,3 +159,9 @@ assert_eq!(v1_iter.next(), Some(&1));
     assert_eq!(v1_iter.next(), Some(&2));
     assert_eq!(v1_iter.next(), Some(&3));
     assert_eq!(v1_iter.next(), None);
+
+    let v1: Vec<i32> = vec![1, 2, 3];
+
+    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+
+    assert_eq!(v2, vec![2, 3, 4]);
