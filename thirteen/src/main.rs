d39311b883f49e61ef7e2c6a572c567ff1a6a7e3
stark5200 <a.taha5200@gmail.com>
2024-02-23 19:15:34 +0100

    commencing 13.2

--- thirteen/src/main.rs
+++ thirteen/src/main.rs
@@ -135,33 +135,51 @@ for val in v1_iter {
     println!("Got: {}", val);
 }
 
-pub trait Iterator {
-    type Item;
-
-    fn next(&mut self) -> Option<Self::Item>;
-
-    // methods with default implementations elided
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
 }
 
-#[test]
-fn iterator_demonstration() {
-    let v1 = vec![1, 2, 3];
-
-    let mut v1_iter = v1.iter();
-
-    assert_eq!(v1_iter.next(), Some(&1));
-    assert_eq!(v1_iter.next(), Some(&2));
-    assert_eq!(v1_iter.next(), Some(&3));
-    assert_eq!(v1_iter.next(), None);
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
 }
 
-assert_eq!(v1_iter.next(), Some(&1));
-    assert_eq!(v1_iter.next(), Some(&2));
-    assert_eq!(v1_iter.next(), Some(&3));
-    assert_eq!(v1_iter.next(), None);
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let v1: Vec<i32> = vec![1, 2, 3];
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
 
-    let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+        let in_my_size = shoes_in_size(shoes, 10);
 
-    assert_eq!(v2, vec![2, 3, 4]);
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                },
+            ]
+        );
+    }
+}
