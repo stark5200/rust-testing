8e90b94f934bc032dacfdaa1dbfe4d0ad1e140fa
stark5200 <a.taha5200@gmail.com>
2024-03-01 22:58:43 +0100

    threads

--- 14/15/fourteenfifteen/src/main.rs
+++ 14/15/fourteenfifteen/src/main.rs
@@ -10,3 +10,20 @@ fn main() {
         tx.send(val).unwrap();
     });
 }
+
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+}
